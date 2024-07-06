d30dc087a296e026d709e85d4b072ce7f8004cc1
AALabonya <1aklimakter@gmail.com>
2024-07-06 11:20:28 -0700

    added mainlayout component

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,9 +1,21 @@
+import { Children } from "react";
 import AdminDashboard from "../pages/admin/AdminDashboard";
+import CreateAdmin from "../pages/admin/CreateAdmin";
 
 const adminPaths = [
   {
     name: "Dashboard",
     path: "dashboard",
     element: <AdminDashboard />,
+  },
+  {
+    name: "User Management",
+    Children: [
+      {
+        name: "Create Admin",
+        path: "create-admin",
+        element: <CreateAdmin />,
+      },
+    ],
   },
 ];
