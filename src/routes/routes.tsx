b03b4b06615e0fe4de9ae14e1e75cca1b7e9ee3e
AALabonya <1aklimakter@gmail.com>
2024-07-06 11:33:31 -0700

    added mainlayout component

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,28 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import Registration from "../pages/Registration";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import CreateFaculty from "../pages/admin/CreateFaculty";
-import CreateAdmin from "../pages/admin/CreateAdmin";
-import CreateStudent from "../pages/admin/CreateStudent";
+import { adminRoutes } from "./admin.routes";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-    ],
   },
 
   {
@@ -36,25 +21,7 @@ const router = createBrowserRouter([
   {
     path: "/admin",
     element: <App />,
-    children: [
-      {
-        path: "dashboard",
-        element: <AdminDashboard />,
-      },
-
-      {
-        path: "createFaculty",
-        element: <CreateFaculty />,
-      },
-      {
-        path: "createAdmin",
-        element: <CreateAdmin />,
-      },
-      {
-        path: "createStudent",
-        element: <CreateStudent />,
-      },
-    ],
+    children: adminRoutes,
   },
 ]);
 
