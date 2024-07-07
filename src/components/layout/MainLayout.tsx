719382f80dbd8430de6fdd4f2128c04dbe266c32
AALabonya <1aklimakter@gmail.com>
2024-07-06 23:00:11 -0700

    added mainlayout component

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { Layout, Menu } from "antd";
+import { Layout } from "antd";
 import {
   UploadOutlined,
   UserOutlined,
@@ -6,42 +6,14 @@ import {
 } from "@ant-design/icons";
 import { createElement } from "react";
 import { Outlet } from "react-router-dom";
-import { adminPaths } from "../../routes/admin.routes";
-import sidebarItemsGenerator from "../../utils/sidebarItemsGenerator";
+
+import Sidebar from "./Sidebar";
 const { Header, Content, Footer, Sider } = Layout;
 
 const MainLayout = () => {
   return (
     <Layout style={{ height: "100vh" }}>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div
-          style={{
-            color: "white",
-
-            height: "4rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <h1>PH Uni</h1>
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          items={sidebarItemsGenerator(adminPaths, "admin")}
-        />
-      </Sider>
+      <Sidebar />
       <Layout>
         <Header style={{ padding: 0 }} />
         <Content style={{ margin: "24px 16px 0" }}>
