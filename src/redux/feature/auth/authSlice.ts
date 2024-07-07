b70b43f60b85bc3d9f9dbd836e08fd063bc8b266
AALabonya <1aklimakter@gmail.com>
2024-07-07 02:15:05 -0700

    added mainlayout component

--- src/redux/feature/auth/authSlice.ts
+++ src/redux/feature/auth/authSlice.ts
@@ -1,5 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
+type TAuthState={
+    user:null || object;
+    token: null || string
+}
 
+const initialState:TAuthState={
+    user:null,
+    token:null
+
+}
 const authSlice = createSlice({
   name: "auth",
 });
