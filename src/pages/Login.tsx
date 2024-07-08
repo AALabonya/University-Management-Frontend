d0e0db1c473cf2ced0ae2468ad20a281a7c4b56a
AALabonya <1aklimakter@gmail.com>
2024-07-08 01:31:54 -0700

    added mainlayout component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,14 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/feature/auth/authApi";
 
 export default function Login() {
   const { register, handleSubmit } = useForm();
+  const [login, { data, error }] = useLoginMutation();
+  console.log("data=>", data);
+  console.log("error=>", error);
+
+  const onSubmit = (data) => {};
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
