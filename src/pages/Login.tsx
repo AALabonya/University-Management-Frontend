a99a9340842745c06b5dc7859cd27f8cd7bcee73
AALabonya <1aklimakter@gmail.com>
2024-07-08 01:40:42 -0700

    added mainlayout component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,11 +4,15 @@ import { useLoginMutation } from "../redux/feature/auth/authApi";
 
 export default function Login() {
   const { register, handleSubmit } = useForm();
+
   const [login, { data, error }] = useLoginMutation();
+
   console.log("data=>", data);
   console.log("error=>", error);
 
-  const onSubmit = (data) => {};
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
