b26e0e72c2fbe383be0e4cfb094e0e587a73fe2e
AALabonya <1aklimakter@gmail.com>
2024-07-08 00:40:38 -0700

    added mainlayout component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,21 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+
 export default function Login() {
-  return <div>Login</div>;
+  const { register, handleSubmit } = useForm();
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="id">ID: </label>
+          <input type="text" id="id" {...register("userId")} />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input type="text" id="password" {...register("password")} />
+        </div>
+        <Button htmlType="submit">Login</Button>
+      </form>
+    </div>
+  );
 }
