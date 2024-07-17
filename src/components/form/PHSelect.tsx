fe70f9637dcb4510d9c2296649c35e1cd518015e
AALabonya <1aklimakter@gmail.com>
2024-07-17 11:12:39 -0700

    create academic semester form

--- src/components/form/PHSelect.tsx
+++ src/components/form/PHSelect.tsx
@@ -1,3 +1,26 @@
-export default function PHSelect() {
-  return <div>PHSelect</div>;
+import { Form, Select } from "antd";
+import { Controller } from "react-hook-form";
+
+type TPHSelectProps = {
+  name: string;
+  label: string;
+  options: { value: string; label: string; disabled?: boolean }[];
+};
+
+export default function PHSelect({ name, label, options }: TPHSelectProps) {
+  return (
+    <Controller
+      name={name}
+      render={({ field }) => (
+        <Form.Item label={label}>
+          <Select
+            style={{ width: "100%" }}
+            {...field}
+            options={options}
+            size="large"
+          />
+        </Form.Item>
+      )}
+    />
+  );
 }
--- src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/AcademicManagement/CreateAcademicSemester.tsx
@@ -1,7 +1,7 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
-import PHInput from "../../../components/form/PHInput";
 import { Button, Col, Flex } from "antd";
+import PHSelect from "../../../components/form/PHSelect";
 
 export default function CreateAcademicSemester() {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
@@ -11,8 +11,7 @@ export default function CreateAcademicSemester() {
     <Flex justify="center" align="center">
       <Col span={6}>
         <PHForm onSubmit={onSubmit}>
-          <PHInput type="text" name="name" label="name" />
-          <PHInput type="text" name="name" label="year" />
+          <PHSelect label="name" />
           <Button htmlType="submit">Submit</Button>
         </PHForm>
       </Col>
