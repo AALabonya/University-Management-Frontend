b252598965eb93e3d44c099f1e3b46050d688e2f
AALabonya <1aklimakter@gmail.com>
2024-07-17 00:25:35 -0700

    added customized form

--- src/components/form/PHForm.tsx
+++ src/components/form/PHForm.tsx
@@ -1,3 +1,12 @@
-export default function PHForm() {
-  return <div>PHForm</div>;
+import { FormProvider, useForm } from "react-hook-form";
+
+export default function PHForm({ onSubmit, children, defaultValue }) {
+  const formConfig: TFormConfig = {};
+
+  const method = useForm(formConfig);
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
 }
