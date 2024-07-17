c087b61456b59ebbabe6fda102d803a717f22564
AALabonya <1aklimakter@gmail.com>
2024-07-17 00:49:42 -0700

    added customized form

--- src/components/form/PHInput.tsx
+++ src/components/form/PHInput.tsx
@@ -1,3 +1,19 @@
-export default function PHInput() {
-  return <div>PHInput</div>;
+import { Input } from "antd";
+import { Controller } from "react-hook-form";
+
+type TInputProps = {
+  type: string;
+  name: string;
+  label?: string;
+};
+export default function PHInput({ type, name, label }: TInputProps) {
+  return (
+    <div style={{ marginBottom: "20px" }}>
+      {label ? label : null}
+      <Controller
+        name={name}
+        render={({ field }) => <Input {...field} type={type} id={name} />}
+      />
+    </div>
+  );
 }
