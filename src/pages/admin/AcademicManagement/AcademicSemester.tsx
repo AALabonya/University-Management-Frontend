221b28c91735b4fe7696a969bd4aafbcd7965f2f
AALabonya <1aklimakter@gmail.com>
2024-07-26 21:54:26 -0700

    added filter

--- src/pages/admin/AcademicManagement/AcademicSemester.tsx
+++ src/pages/admin/AcademicManagement/AcademicSemester.tsx
@@ -1,4 +1,4 @@
-import { Table } from "antd";
+import { Button, Table } from "antd";
 import type { TableColumnsType, TableProps } from "antd";
 import { useGetAllSemestersQuery } from "../../../redux/feature/admin/academicManagementSemester.api";
 import { TAcademicSemester } from "../../../types/academicManagement.type";
@@ -14,7 +14,12 @@ export type TTableData = Pick<
   "name" | "year" | "startMonth" | "endMonth"
 >;
 export default function AcademicSemester() {
-  const { data: semesterData } = useGetAllSemestersQuery(undefined);
+  const { data: semesterData } = useGetAllSemestersQuery([
+    {
+      name: "year",
+      value: "2024",
+    },
+  ]);
   console.log(semesterData);
 
   const tableData = semesterData?.data?.map((item) => ({
@@ -28,23 +33,62 @@ export default function AcademicSemester() {
   const columns: TableColumnsType<TTableData> = [
     {
       title: "Name",
+      key: "name",
       dataIndex: "name",
-    },
-    {
-      title: "Code",
-      dataIndex: "code",
+      filters: [
+        {
+          text: "Autumn",
+          value: "Autumn",
+        },
+        {
+          text: "Fall",
+          value: "Fall",
+        },
+        {
+          text: "Summer",
+          value: "Summer",
+        },
+      ],
     },
     {
       title: "Year",
+      key: "year",
       dataIndex: "year",
+      filters: [
+        {
+          text: "2024",
+          value: "2024",
+        },
+        {
+          text: "2025",
+          value: "2025",
+        },
+        {
+          text: "2026",
+          value: "2026",
+        },
+      ],
     },
     {
-      title: "startMonth",
+      title: "Start Month",
+      key: "startMonth",
       dataIndex: "startMonth",
     },
     {
-      title: "endMonth",
+      title: "End Month",
+      key: "endMonth",
       dataIndex: "endMonth",
+    },
+    {
+      title: "Action",
+      key: "x",
+      render: () => {
+        return (
+          <div>
+            <Button>Update</Button>
+          </div>
+        );
+      },
     },
   ];
 
