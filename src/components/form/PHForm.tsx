bd587ef9ba2521276e328f33446df0024e9aa7d7
AALabonya <1aklimakter@gmail.com>
2024-07-29 00:55:32 -0700

    reset form done

--- src/components/form/PHForm.tsx
+++ src/components/form/PHForm.tsx
@@ -31,9 +31,14 @@ export default function PHForm({
     formConfig["resolver"] = resolver;
   }
   const methods = useForm(formConfig);
+
+  const submit = (data) => {
+    onSubmit(data);
+    methods.reset();
+  };
   return (
     <FormProvider {...methods}>
-      <Form layout="vertical" onFinish={methods.handleSubmit(onSubmit)}>
+      <Form layout="vertical" onFinish={methods.handleSubmit(submit)}>
         {children}
       </Form>
     </FormProvider>
