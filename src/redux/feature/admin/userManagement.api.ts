f5c8e3031b112093e928c6f3e42b5ac8f3bde4a5
AALabonya <1aklimakter@gmail.com>
2024-07-28 20:23:52 -0700

    create usermanagementapi

--- src/redux/feature/admin/userManagement.api.ts
+++ src/redux/feature/admin/userManagement.api.ts
@@ -22,6 +22,7 @@ const userManagementApi = baseApi.injectEndpoints({
     addStudent: builder.mutation({
       query: (data) => ({
         url: "/users/create-student",
+        method: "POST",
       }),
     }),
   }),
