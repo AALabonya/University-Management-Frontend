f68acfbb69898d8898f176f68d0cb885f51beaaa
AALabonya <1aklimakter@gmail.com>
2024-07-31 20:22:36 -0700

    show student data

--- src/pages/admin/userManagement/StudentData.tsx
+++ src/pages/admin/userManagement/StudentData.tsx
@@ -27,60 +27,21 @@ export default function StudentData() {
   const tableData = semesterData?.data?.map((item) => ({
     key: item._id,
     name: item.name,
-    code: item.code,
-    year: item.year,
-    startMonth: item.startMonth,
-    endMonth: item.endMonth,
+    id: item.id,
   }));
   const columns: TableColumnsType<TTableData> = [
     {
       title: "Name",
       key: "name",
       dataIndex: "name",
-      filters: [
-        {
-          text: "Autumn",
-          value: "Autumn",
-        },
-        {
-          text: "Fall",
-          value: "Fall",
-        },
-        {
-          text: "Summer",
-          value: "Summer",
-        },
-      ],
     },
+
     {
-      title: "Year",
-      key: "year",
-      dataIndex: "year",
-      filters: [
-        {
-          text: "2024",
-          value: "2024",
-        },
-        {
-          text: "2025",
-          value: "2025",
-        },
-        {
-          text: "2026",
-          value: "2026",
-        },
-      ],
+      title: "Roll No.",
+      key: "id",
+      dataIndex: "id",
     },
-    {
-      title: "Start Month",
-      key: "startMonth",
-      dataIndex: "startMonth",
-    },
-    {
-      title: "End Month",
-      key: "endMonth",
-      dataIndex: "endMonth",
-    },
+
     {
       title: "Action",
       key: "x",
